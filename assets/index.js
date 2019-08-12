70271c1e5fef10656b9662858d539f9c3fdfae8c
a <53735966+learnsanskrit@users.noreply.github.com>
2019-08-12 16:15:48 +0530

    update

---
+++ assets/index.js
@@ -0,0 +1,85 @@
+var inputtext='';
+function back(){
+  inputtext=inputtext.slice(0,-1);
+  document.getElementById("inputplace").innerHTML=inputtext;
+}
+function type(e) {
+  inputtext=inputtext.concat(e.target.innerHTML);
+  document.getElementById("inputplace").innerHTML=inputtext;
+}
+function types(e) {
+  inputtext=inputtext.concat(e.target.innerHTML);
+  document.getElementById("inputplace").innerHTML=inputtext;
+  document.getElementById("shiftkeyboard").style.display="none";
+}
+var w=document.getElementById("primarykeyboard").clientWidth;
+var letters=['ṃ','ḥ','e','r','t','y','u','i','o','p','a','s','d','ṛ','g','h','j','k','l','ś','ṣ','c','v','b','n','m'];
+var sletters=['ṃ','ḥ','e','r','ṭ','y','ū','ī','o','p','ā','s','ḍ','ṝ','g','h','ñ','k','l','ś','ṣ','c','v','b','ṇ','ṅ'];
+var keysdeclaration='';
+var skeysdeclaration='';
+var shiftkeydeclaration='<div class=\"keys\" style=\"width: ' + 3.0*0.5*w/10.0 + 'px; top: ' + 1.3*w/5.0 + 'px; padding-top: 0;\" id=\"shiftsq\"><svg height=\"' + 1.3*w/10.0 + 'px\" width=\"' + w/10.0 + 'px\"><circle cx=\"' + 0.5*w/10.0 + 'px\" cy=\"' + 0.5*1.3*w/10.0 + 'px\" r=\"' + 0.5*w/10.0 + 'px\" fill=\"gray\"></svg></div>';
+var sshiftkeydeclaration='<div class=\"keys\" style=\"width: ' + 3.0*0.5*w/10.0 + 'px; top: ' + 1.3*w/5.0 + 'px; padding-top: 0;\" id=\"sshiftsq\"><svg height=\"' + 1.3*w/10.0 + 'px\" width=\"' + w/10.0 + 'px\"><circle cx=\"' + 0.5*w/10.0 + 'px\" cy=\"' + 0.5*1.3*w/10.0 + 'px\" r=\"' + 0.5*w/10.0 + 'px\" fill=\"gray\"></svg></div>';
+var backkeydeclaration='<div class=\"keys\" style=\"width: ' + 3.0*0.5*w/10.0 + 'px; right:0; top: ' + 1.3*w/5.0 + 'px; padding-top: 0;\" id=\"backsq\"><svg height=\"' + 1.3*w/10.0 + 'px\" width=\"' + w/10.0 + 'px\"><circle cx=\"' + 0.5*w/10.0 + 'px\" cy=\"' + 0.5*1.3*w/10.0 + 'px\" r=\"' + 0.5*w/10.0 + 'px\" fill=\"gray\"></svg></div>';
+var sbackkeydeclaration='<div class=\"keys\" style=\"width: ' + 3.0*0.5*w/10.0 + 'px; right:0; top: ' + 1.3*w/5.0 + 'px; padding-top: 0;\" id=\"sbacksq\"><svg height=\"' + 1.3*w/10.0 + 'px\" width=\"' + w/10.0 + 'px\"><circle cx=\"' + 0.5*w/10.0 + 'px\" cy=\"' + 0.5*1.3*w/10.0 + 'px\" r=\"' + 0.5*w/10.0 + 'px\" fill=\"gray\"></svg></div>';
+
+for(i=0;i<letters.length;i++){
+  keysdeclaration=keysdeclaration+"<div class=\"keys\" id=\""+ letters[i] + "sq\"><div class=\"text\" id=\""+letters[i]+"key\">"+letters[i]+"</div></div>";
+  skeysdeclaration=skeysdeclaration+"<div class=\"keys\" id=\"s"+ sletters[i] + "sq\"><div class=\"text\" id=\"s"+sletters[i]+"key\">"+sletters[i]+"</div></div>";
+}
+skeysdeclaration=skeysdeclaration+sshiftkeydeclaration+sbackkeydeclaration;
+keysdeclaration=keysdeclaration+shiftkeydeclaration+backkeydeclaration;
+
+document.getElementById("primarykeyboard").innerHTML=keysdeclaration;
+document.getElementById("shiftkeyboard").innerHTML=skeysdeclaration;
+document.getElementById("shiftsq").addEventListener("click",function() {document.getElementById("shiftkeyboard").style.display="inline";});
+document.getElementById("sshiftsq").addEventListener("click",function() {document.getElementById("shiftkeyboard").style.display="none";});
+document.getElementById("backsq").addEventListener("click",back);
+document.getElementById("sbacksq").addEventListener("click",back);
+for(i=0;i<letters.length;i++){
+  var csq=document.getElementById(letters[i]+"sq");
+  var cssq=document.getElementById("s"+sletters[i]+"sq");
+  var ck=document.getElementById(letters[i]+"key");
+  var csk=document.getElementById("s"+sletters[i]+"key");
+  if(i<10){
+    csq.style.left=i*w/10.0+"px";
+    cssq.style.left=i*w/10.0+"px";
+  }
+  else if(i<19){
+    csq.style.left=(i-9.5)*w/10.0+"px";
+    csq.style.top=w*1.3/10.0+"px";
+    cssq.style.left=(i-9.5)*w/10.0+"px";
+    cssq.style.top=w*1.3/10.0+"px";
+  }
+  else{
+    csq.style.left=(i-17.5)*w/10.0+"px";
+    csq.style.top=w*1.3/5.0+"px";
+    cssq.style.left=(i-17.5)*w/10.0+"px";
+    cssq.style.top=w*1.3/5.0+"px";
+  }
+  ck.style.bottom=0.0+"px";
+  ck.style.lineHeight=Math.floor(w*1.3/10.0)+"px";
+  ck.style.width=Math.floor(w/10.0)+"px";
+  ck.style.fontSize=Math.floor(w/17.0)+"px";
+  csq.addEventListener("click",type);
+  
+  csk.style.bottom=0.0+"px";
+  csk.style.lineHeight=Math.floor(w*1.3/10.0)+"px";
+  csk.style.width=Math.floor(w/10.0)+"px";
+  csk.style.fontSize=Math.floor(w/17.0)+"px";
+  cssq.addEventListener("click",types);
+}
+
+/*document.getElementById("shiftsq").style.left=0.5*w/10.0+"px";;
+*/
+function openkeyboard() {
+  document.getElementById("primarykeyboard").style.height = w*1.3*0.3+"px";
+  document.getElementById("shiftkeyboard").style.height = w*1.3*0.3+"px";
+}
+
+/* Set the width of the side navigation to 0 */
+function closekeyboard() {
+  document.getElementById("primarykeyboard").style.height = "0";
+  document.getElementById("shiftkeyboard").style.height="0";
+}
+
+openkeyboard();
--- service-worker.js
+++ service-worker.js
@@ -1,74 +1,89 @@
-const CACHE_NAME = 'mpwa-cache-v1';
-const urlsToCache = [
-  '/',
-  '/assets/style.css',
-  '/assets/index.js',
-  '/assets/prism.js',
-  '/assets/img/pwa-stats.png',
-  '/assets/img/bing-store.jpg',
-];
+console.log('Hello from service-worker.js');
 
-// Listen for the install event, which fires when the service worker is installing
-self.addEventListener('install', event => {
-  // Ensures the install event doesn't complete until after the cache promise resolves
-  // This is so we don't move on to other events until the critical initial cache is done
-  event.waitUntil(
-    // Open a named cache, then add all the specified URLs to it
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
-});
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js');
 
-// Listen for the activate event, which is fired after installation
-// Activate is when the service worker actually takes over from the previous
-// version, which is a good time to clean up old caches
-self.addEventListener('activate', event => {
-  console.log('Finally active. Ready to serve!');
-  event.waitUntil(
-    // Get the keys of all the old caches
-    caches
-      .keys()
-      // Ensure we don't resolve until all the promises do (i.e. each key has been deleted)
-      .then(keys =>
-        Promise.all(
-          keys
-            // Remove any cache that matches the current cache name
-            .filter(key => key !== CACHE_NAME)
-            // Map over the array of old cache names and delete them all
-            .map(key => caches.delete(key))
-        )
-      )
-  );
-});
+if (workbox) {
+  console.log(`Yay! Workbox is loaded 🎉`);
+} else {
+  console.log(`Boo! Workbox didn't load 😬`);
+}
 
-// Listen for browser fetch events. These fire any time the browser tries to load
-// any outside resources
-self.addEventListener('fetch', function(event) {
-  // This lets us control the response
-  // We pass in a promise that resolves with a response object
-  event.respondWith(
-    // Check whether we have a matching response for this request in our cache
-    caches.match(event.request).then(response => {
-      // It's in the cache! Serve the response straight from there
-      if (response) {
-        console.log('Serving response from the cache');
-        return response;
-      }
-      // If it's not in the cache we make a fetch request for the resource
-      return (
-        fetch(event.request)
-          // Then we open our cache
-          .then(response => caches.open(CACHE_NAME))
-          // Then we put the request into the cache, so we have it offline next time
-          .then(cache => {
-            // We have to clone the response as response streams can only be read once
-            // This way we can put one copy in the cache and return the other to the browser
-            cache.put(event.request, response.clone());
-            return response;
-          })
-          .catch(response => {
-            console.log('Fetch failed, sorry.');
-          })
-      );
-    })
-  );
+workbox.routing.registerRoute(
+  // Cache CSS files.
+  /\.(?:js|css)$/,
+  // Use cache but update in the background.
+  new workbox.strategies.NetworkFirst(){
+    // Use a custom cache name.
+    cacheName: 'css-js',
+  })
+);
+
+workbox.routing.registerRoute(
+  /\.(?:png|gif|jpg|jpeg|webp|svg)$/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'images',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+// Cache the Google Fonts stylesheets with a stale-while-revalidate strategy.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.googleapis\.com/,
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'google-fonts-stylesheets',
+  })
+);
+
+// Cache the underlying font files with a cache-first strategy for 1 year.
+workbox.routing.registerRoute(
+  /^https:\/\/fonts\.gstatic\.com/,
+  new workbox.strategies.CacheFirst({
+    cacheName: 'google-fonts-webfonts',
+    plugins: [
+      new workbox.cacheableResponse.Plugin({
+        statuses: [0, 200],
+      }),
+      new workbox.expiration.Plugin({
+        maxAgeSeconds: 60 * 60 * 24 * 365,
+        maxEntries: 30,
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  /.*(?:googleapis|gstatic)\.com/,
+  new workbox.strategies.StaleWhileRevalidate(),
+);
+
+workbox.routing.registerRoute(
+  // Custom `matchCallback` function
+  ({event}) => event.request.destination === 'audio',
+  new workbox.strategies.CacheFirst({
+    cacheName: 'audio',
+    plugins: [
+      new workbox.expiration.Plugin({
+        maxEntries: 60,
+        maxAgeSeconds: 30 * 24 * 60 * 60, // 30 Days
+      }),
+    ],
+  })
+);
+
+workbox.routing.registerRoute(
+  new RegExp('/static/'),
+  new workbox.strategies.StaleWhileRevalidate({
+    cacheName: 'my-cache', // Use the same cache name as before.
+  })
+);
+
+addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    skipWaiting();
+  }
 });
