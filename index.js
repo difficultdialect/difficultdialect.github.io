ff71795018ede6619038a2550b537cd4a42f6826
learnsanskrit <53735966+learnsanskrit@users.noreply.github.com>
2019-08-20 12:36:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -241,7 +241,7 @@ for(i=0;i<letters.length;i++){
 function openkeyboard() {
   document.getElementById("primarykeyboard").style.height = w*1.3*0.3+"px";
   document.getElementById("shiftkeyboard").style.height = w*1.3*0.3+"px";
-  document.getElementById("space").style.height=document.getElementsByTagName("body")[0].clientHeight - w*1.3*0.3+"px";
+  document.getElementById("space").style.height=(window.innerHeight - w*1.3*0.3)+"px";
   openedkb=1;
 }
 
@@ -249,7 +249,7 @@ function openkeyboard() {
 function closekeyboard() {
   document.getElementById("primarykeyboard").style.height = "0";
   document.getElementById("shiftkeyboard").style.height="0";
-  document.getElementById("space").style.height=document.getElementsByTagName("body")[0].clientHeight;
+  document.getElementById("space").style.height=window.innerHeight+"px";
   openedkb=0;
 }
 function animateCSS(element, animationName, callback) {
