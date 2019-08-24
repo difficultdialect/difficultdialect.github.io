f4c2e399598b172cd81e8bf867cd448f6267dff6
a <53735966+learnsanskrit@users.noreply.github.com>
2019-08-24 20:45:44 +0530

    update

--- index.js
+++ index.js
@@ -155,6 +155,7 @@ var imgereplace="#i";
 
 var inputdeclaration="<div id=\"inputplace\"><span style=\"color: #ffffff\">.</span></div>";
 function next(){
+  document.getElementById("shiftkeyboard").style.display="none";
   if(slidei==sliden){slidei--;}
   document.getElementById("space").style.display="none";
   console.log("next"); 
