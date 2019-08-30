21320959bbbf419d6208e0dcbb478d4ed054e614
learnsanskrit <53735966+learnsanskrit@users.noreply.github.com>
2019-08-30 12:46:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -57,7 +57,22 @@ var inputtext='';
 var nimages=0;
 var q="";
 var slideover=0;
+function onSignIn(googleUser) {
+        // Useful data for your client-side scripts:
+        var profile = googleUser.getBasicProfile();
+        console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+        console.log('Full Name: ' + profile.getName());
+        console.log('Given Name: ' + profile.getGivenName());
+        console.log('Family Name: ' + profile.getFamilyName());
+        console.log("Image URL: " + profile.getImageUrl());
+        console.log("Email: " + profile.getEmail());
+
+        // The ID token you need to pass to your backend:
+        var id_token = googleUser.getAuthResponse().id_token;
+        console.log("ID Token: " + id_token);
+      }
 var slides=[
+	{input:0, q:"Sign in to contiunue learning Sanskrit. <div class=\"g-signin2\" data-onsuccess=\"onSignIn\"></div>"},
 	{input:1, q:"[patram]		एतत्पत्रम्। etatpatram. (This is a leaf.)<br>एतत्किम्? etatkim? (What is this?)<p>Type: patram.</p>", a:"patram"},
 	{input:1, q:"[patram2]		एतत्पात्रम्। etatpātram.<br>etatkim?<p>Type the answer.</p>", a:"pātram"},
 	{input:1, q:"[jalam3]		एतज्जलम्। etajjalam.<br>etatkim?<p>Type: jalam.</p>", a:"jalam"},
