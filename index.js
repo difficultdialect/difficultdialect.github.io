4882cbff126b0aa423894f53ef8e31d0e87baae1
learnsanskrit <53735966+learnsanskrit@users.noreply.github.com>
2019-08-30 17:09:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -60,10 +60,6 @@ var slideover=0;
 function renderButton() {
       gapi.signin2.render('my-signin2', {
         'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
         'onsuccess': onSignIn,
       });
     }
