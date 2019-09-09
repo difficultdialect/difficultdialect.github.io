808bda41f28289a2e2e40ecf3717bb33efe91fa9
learnsanskrit <53735966+learnsanskrit@users.noreply.github.com>
2019-09-09 16:55:07 +0530

    Update main.js

--- main.js
+++ main.js
@@ -49,7 +49,7 @@ var record={skills:[], /* Type: {skillName, proficiency, interval}*/
 var recordupdate;	// update record based on Date.now(), record and last response
 
 var theme={kbd:{keyh:1.5/*key aspect ratio*/,h:0.4/*number of unit key heights / 10*/},
-	   bgcolor: '#ffffff',textcolor: '#000000',fonts:[{f:'Martel:400, 700:devanagari,latin-ext'},{f:'Mukta:400, 700',t:'āḍḥīḷḹṃṇñṅṛṝṣśṭūertyuiopasdghjklcvbnm.?'}]};
+	   bgcolor: '#ffffff',textcolor: '#000000',fonts:[{f:'Martel:400, 700:devanagari,latin-ext',t:'āḍḥīḷḹṃṇñṅṛṝṣśṭūertyuiopasdghjklcvbnm.?'},{f:'Mukta:400, 700',t:'āḍḥīḷḹṃṇñṅṛṝṣśṭūertyuiopasdghjklcvbnm.?'}]};
 var display={
 	w:document.body.offsetWidth, h:document.body.offsetHeight, kbd:{lefts:[], tops:[]},
 	populate:function(_theme){},
