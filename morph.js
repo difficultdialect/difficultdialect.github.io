461eb10f3bb9d3b22a4f3012b7c00a9b3f18f7d9
verbalsanskrit <53735966+verbalsanskrit@users.noreply.github.com>
2019-09-24 17:22:12 +0530

    Update morph.js

--- morph.js
+++ morph.js
@@ -1,1 +1,22 @@
+function parse(s){
+	let r=[],d=[];
+	for(let l of s){
+		if(l.includes('=')){
+			const i=l.indexOf('=');
+			r.push({f:l.slice(0,i),t:l.slice(i+1)});
+		}
+		else if(l.includes(':')){
+			const i=l.indexOf(':');
+			d.push({f:l.slice(0,i),t:l.slice(i+1)});
+		}
+		else replaceRecurse(r,l);
+	}
+}
 
+function replaceRecurse(rl,s){
+	for(let r of rl){
+		const c=s.replace(r.f,r.t);
+		console.log(c);
+		if(c!==s) replaceRecurse(rl,c);
+	}
+}
